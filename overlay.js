04708e03389f9e84770de7edcc07aa0b67603a81
ranuncudahlia <ranuncudahlia@gmail.com>
2021-01-24 19:59:19 -0600

    Update overlay.js

--- overlay.js
+++ overlay.js
@@ -1,1 +1,26 @@
+function mutationCheck() {
 
+    var melaCheck = document.getElementById("melaCheck").checked;
+    var albinoCheck = document.getElementById("albinoCheck").checked;
+    var offspringBase = document.getElementById("offspringBase")
+    var offspringImport = document.getElementById("offspringImage");
+    var importText = document.getElementById("importText");
+    
+    if (melaCheck == true) {
+        if (albinoCheck == true) {
+            window.alert("Please select only one mutation type.");
+            }
+        else {
+            offspringBase.src = "melareference.png";
+        }
+    }
+    else if (albinoCheck == true) {
+        offspringBase.src = "albinoreference.png";
+    }
+    else {
+        window.alert("You must select which mutation you are checking for. Please select one of the checkboxes above.");
+        }
+
+    offspringImport.src = importText
+    
+}
